84b5ac53207bf8679413957e80dde3359da62646
uehara <b1015062@fun.ac.jp>
2017-03-25 17:59:21 +0900

    簡単な認証システムを追加

--- serverctl/urls.py
+++ serverctl/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 
 from .views import index
+from .views import login_view
+from .views import signup
 
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^login/$', login_view, name='login'),
+    url(r'^signup/$', signup, name='signup'),
 ]
--- serverctl/views.py
+++ serverctl/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     return render(request, 'serverctl/index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['email'], password=request.POST['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('serverctl:index'))
+            return redirect(reverse('serverctl:login'))
+    logout(request)
+    return render(request, 'serverctl/login.html')
+
+
+def signup(request):
+    print(request.POST)
+    if request.method == 'POST':
+        user = User.objects.create_user(username=request.POST['email'], password=request.POST['password'])
+        print(user)
+        authenticate(username=request.POST['email'], password=request.POST['password'])
+        login(request, user)
+        return redirect(reverse('serverctl:index'))
+    return redirect('/login/')
