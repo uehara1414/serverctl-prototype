881bdd96d683370bf095d4e3840df9717b47be0a
uehara <b1015062@fun.ac.jp>
2017-03-25 23:27:27 +0900

    admin に追加

--- serverctl/admin.py
+++ serverctl/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Game
+from .models import GameServer
+from .models import GameServerGroup
+from .models import Player
 
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(GameServerGroup)
+admin.site.register(GameServer)
+admin.site.register(Player)
