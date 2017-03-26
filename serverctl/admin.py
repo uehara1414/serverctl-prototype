56515cec7783c93f42039b097e524f4f3900161a
uehara <b1015062@fun.ac.jp>
2017-03-26 16:37:38 +0900

    register to admin

--- serverctl/admin.py
+++ serverctl/admin.py
@@ -3,9 +3,13 @@ from .models import Game
 from .models import GameServer
 from .models import GameServerGroup
 from .models import Player
+from .models import Payments
+from .models import ServerHistory
 
 # Register your models here.
 admin.site.register(Game)
 admin.site.register(GameServerGroup)
 admin.site.register(GameServer)
 admin.site.register(Player)
+admin.site.register(Payments)
+admin.site.register(ServerHistory)
